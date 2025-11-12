342c5fcc65e49ef8bd7bd81bfa771a8e616eba5e
gabriel crystian <gabrielcrystian30@gmail.com>
2025-11-12 19:31:00 -0300

    mudei
    acho que tenho que criar cada pasta

---
+++ Untitled- teste 1.c
@@ -0,0 +1,16 @@
+// novo projeto 
+#include <stdio.h>
+
+int mian(){
+    int numero = 5, resultado;
+
+    resultado = numero % 2;
+
+    printf("a variavel resultado é: %d\n", resultado);
+
+    if (resultado == 0){
+        
+        printf (" o numero é par\n");
+    }
+
+}
---
+++ Untitled-1.c
@@ -0,0 +1,13 @@
+// verificando idade 
+#include <stdio.h>
+
+int main() {
+    int idade1 = 25;
+    int idade2 = 30;
+    
+    if (idade1 != idade2){
+        printf(" as idades sao diferentes\n");
+
+    }
+
+}
